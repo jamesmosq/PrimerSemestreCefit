6f5589b9611c642004430456aa9bb1acdb21d37b
jamesmosq <161092898+jamesmosq@users.noreply.github.com>
2024-06-01 08:26:52 -0500

    Add files via upload

---
+++ con.py
@@ -0,0 +1,20 @@
+import mysql.connector
+user = 'root'
+password = ''
+host = 'localhost'
+database = 'northwind'
+try:
+    conexion = mysql.connector.connect(
+        host=host,
+        user=user,
+        passwd = "",
+        database=database,
+    )
+    print("Conexión exitosa")
+except mysql.connector.Error as error:
+    print("Error de conexión:", error)
+finally:
+    # Si la conexión se estableció, la cerramos
+    if 'conexion' in locals() or 'conexion' in globals():
+        conexion.close()
+# pip install mysql-connector-python    conexion
