1ca1862f70a5ff3a8944b403666bbd8a2b3df44f
jamesmosq <161092898+jamesmosq@users.noreply.github.com>
2024-05-31 19:07:53 -0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+p1 = Person("John", 36)
+p2 = Person("Ana", 10)
+
+print(p1.name, p1.age)
+print(p2.name, p2.age)
+# print(p1.age)
